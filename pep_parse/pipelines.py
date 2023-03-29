86a02c676be602de25860c9edc7280163860a008
Андрей Куликов <a.kulikov1711@gmail.com>
2023-03-29 12:24:24 +1000

    Пройдено ревью

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -5,19 +5,20 @@ from .constants import BASE_DIR
 
 
 class PepParsePipeline:
-
-    def open_spider(self, spider) -> None:
-        global peps_count
-        peps_count = {
+    def __init__(self):
+        self.peps_count = {
             'Total': 0,
         }
 
+    def open_spider(self, spider) -> None:
+        pass
+
     def process_item(self, item, spider) -> object:
         status = item.get('status')
-        if status not in peps_count:
-            peps_count[status] = 0
-        peps_count[status] += 1
-        peps_count['Total'] += 1
+        if status not in self.peps_count:
+            self.peps_count[status] = 0
+        self.peps_count[status] += 1
+        self.peps_count['Total'] += 1
         return item
 
     def close_spider(self, spider) -> None:
@@ -27,7 +28,7 @@ class PepParsePipeline:
         file_path = results_dir / file_name
         with open(file_path, mode='w', newline='', encoding='utf-8') as f:
             f.write('Статус,Количество\n')
-            fieldnames = peps_count.keys()
+            fieldnames = self.peps_count.keys()
             writer = csv.DictWriter(f, fieldnames=fieldnames)
             writer.writeheader()
-            writer.writerow(peps_count)
+            writer.writerow(self.peps_count)
